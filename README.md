0737e6f4666befece1d3c9e7e7e3f251c3a5c0b2
shibayu <shibayu2002@gmail.com>
2020-02-08 13:34:26 +0900

    PJHttp追加

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # PJUtil
 ## Overview
  PLUtil is a lightweight JavaScript library. 
- The functions used in any application, such as the display of loading images, are summarized.
-
+ 1. PJLoader
+   The functions used in any application, such as the display of loading images, are summarized.
+ 2. PJHttp
+   Controls HTTP requests.
+   
 ## Licence
  The Open PJUtil is released under the Modified BSD license (see http://www.opensource.org/). Using and distributing this software is free (without restriction including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of this work, and to permit persons to whom this work is furnished to do so) subject to the conditions in the following license:
  
  Copyright (c) 2020  Yu.Shiba
 
 ## How to use
- Details are described on my blog.
- <a href="http://piyonekochannel.com/entry/2020/02/03/162716" target="_blank">http://piyonekochannel.com/entry/2020/02/03/162716</a>
+ 1. PJLoader
+   Details are described on my blog.
+   <a href="http://piyonekochannel.com/entry/2020/02/03/162716" target="_blank">http://piyonekochannel.com/entry/2020/02/03/162716</a>
+ 2. PJHttp
+ 
--- js/pjutil.js
+++ js/pjutil.js
@@ -1,5 +1,6 @@
-// Loading画像制御クラス ///////////////////////////////////////////////////////
+// PJLoader ～Loading画像制御クラス～ //////////////////////////////////////////
 // コンストラクタ
+// [引数] msg:ローディング中に表示するメッセージ
 PJLoader = function(msg){
   $('<img id="pj_loading" src="img/loading.gif" title="loading" alt="loading">').appendTo('body');
   $('<div id="pj_loading_text">' + msg + '</div>').appendTo('body');
@@ -20,4 +21,94 @@ PJLoader.prototype.stop = function() {
   $("#pj_loading").fadeOut(this.speed);
   $("#pj_loading_text").fadeOut(this.speed);
 };
+
+// PJHttp ～HTTPリクエスト制御クラス～ /////////////////////////////////////////
+PJHttp = function(){
+  this.lock = false;
+};
+
+// プロパティ
+PJHttp.prototype.url = null;  // URL
+PJHttp.prototype.responseType = "text";  // レスポンスType
+PJHttp.prototype.callback = null;  // コールバック関数(引数：response)
+PJHttp.prototype.callbackError = null;  // エラーコールバック関数(引数：status, statusText)
+PJHttp.prototype.loader = null;  // PJLoader
+PJHttp.prototype.lockCheck = true;
+PJHttp.prototype.uploadEventListener = null;
+
+PJHttp.prototype.sendRequest = function(params, aType) {
+  var formData = new FormData();
+  if(params != null){
+    for(key in params){
+     formData.append(key, params[key]);
+    }
+  }
+
+  var xhr = new XMLHttpRequest();
+  if(this.uploadEventListener != null){
+    xhr.upload.addEventListener("progress", this.uploadEventListener, false);
+  }
+  var parent = this;
+
+  xhr.onload = function (e) {
+    try{
+      if ((xhr.readyState === 4) && (xhr.status === 200)) {
+        if(parent.callback != null){
+          parent.callback(xhr.response);
+          return;
+        }
+      }
+      
+      if(parent.callbackError != null){
+        parent.callbackError(xhr.status, xhr.statusText);
+      }else{
+        window.alert("エラーが発生しました。" + xhr.status + ":" + xhr.statusText);
+      }
+    }catch(e){
+      window.alert("エラーが発生しました。" + e);
+    }finally{
+      if(parent.loader != null){
+        parent.loader.stop();
+      }
+      parent.lock = false;
+    }
+  };
+  xhr.overrideMimeType('text/plain; charset=UTF-8');
+  xhr.open(aType, this.url, true);
+  xhr.responseType = this.responseType;
+  xhr.send(formData);
+};
+
+// メソッド
+// Postリクエスト
+// [引数] params:リクエストパラメタ(key-valueのマップデータ)
+PJHttp.prototype.post = function(params = null) {
+  if(this.lockCheck && this.lock){
+    window.alert("ただいま処理中です。");
+    return;
+  }
+  this.lock = true;
+  if(this.loader != null){
+    this.loader.start();
+  }
+  var that = this;
+  setTimeout(function(){that.sendRequest(params, "POST")}, 0);
+}
+
+// メソッド
+// Getリクエスト
+// [引数] params:リクエストパラメタ(key-valueのマップデータ)
+PJHttp.prototype.get = function(params = null) {
+  if(this.lockCheck && this.lock){
+    window.alert("ただいま処理中です。");
+    return;
+  }
+  this.lock = true;
+  if(this.loader != null){
+    this.loader.start();
+  }
+  var that = this;
+  setTimeout(function(){that.sendRequest(params, "GET")}, 0);
+}
+
 ///////////////////////////////////////////////////////////////////////////////
